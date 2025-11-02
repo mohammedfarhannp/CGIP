7d91380f890ea33b99b80b0483236ae1447e66bc
mohammedfarhannp <m.farhan.np@gmail.com>
2025-11-02 23:55:51 +0530

    Migration to python

---
+++ Bresenhams-Line-Algorithm/main.py
@@ -0,0 +1,42 @@
+import turtle
+
+def Bresenhams_Line_Algo(x1, y1, x2, y2):
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    
+    sx = 1 if x2 > x1 else -1
+    sy = 1 if y2 > y1 else -1
+    
+    err = dx - dy
+    
+    tmp = []
+    
+    while True:
+        tmp.append((x1, y1))
+        
+        if (x1 == x2) and (y1 == y2):break
+        
+        e2 = 2 * err
+        
+        if(e2 > -dy):
+            err -= dy
+            x1 += sx
+            
+        if(e2 < dx):
+            err += dx
+            y1 += sy
+
+    return tmp
+
+pts = Bresenhams_Line_Algo(0, 0, 30, 100)
+
+turtle.speed(0)
+turtle.hideturtle()
+turtle.pencolor("blue")
+turtle.penup()
+
+for x, y in pts:
+    turtle.goto(x, y)
+    turtle.dot(2)
+
+turtle.done()
--- DDA/main.py
+++ DDA/main.py
@@ -3,13 +3,33 @@ from subprocess import check_output
 
 import turtle
 
-# Get Points from C Code Compiled (For Faster Calculation)
-Points_to_Plot = eval(
-        check_output(
-            ['.\gp.exe', '10', '12', '40', '30']
-        ).decode()
-    )
+# Vars
+Points_to_Plot = []
 
+# Functions
+def DDA(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    
+    k = abs(dx) if abs(dx) > abs(dy) else abs(dy)
+    
+    x_inc = abs(dx) / k
+    y_inc = abs(dy) / k
+    
+    x = x1
+    y = y1
+    
+    tmp = [(int(x), int(y))]
+    for i in range(k):
+        x += x_inc
+        y += y_inc
+        tmp.append((int(x),int(y)))
+    return tmp
+
+# Main
+Points_to_Plot = DDA(10, 10, 20, 40)
+
+# Graphics
 turtle.speed(0)
 turtle.hideturtle()
 turtle.pencolor("green")
